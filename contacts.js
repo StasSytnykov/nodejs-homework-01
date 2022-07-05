440b9c630a29ab79d5b4b12083a4b2f404c0bc0b
Stas Sytnykov <stas82kak05@gmail.com>
2022-07-05 20:40:03 +0300

    add start variables and func

---
+++ contacts.js
@@ -0,0 +1,28 @@
+const fs = require("fs");
+
+/*
+ * Раскомментируй и запиши значение
+ * const contactsPath = ;
+ */
+
+// TODO: задокументировать каждую функцию
+function listContacts() {
+  // ...твой код
+}
+
+function getContactById(contactId) {
+  // ...твой код
+}
+
+function removeContact(contactId) {
+  // ...твой код
+}
+
+function addContact(name, email, phone) {
+  // ...твой код
+}
+
+fs.readFile("./db/contacts.json", (error, data) => {
+  console.log(error);
+  console.log(data);
+});
